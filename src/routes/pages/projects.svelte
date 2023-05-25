4abe1bf189cf87194ba4e91dd5b9fb986e3ed1d4
Sreejith KS <sreejithksgupta2255@gmail.com>
2023-05-25 16:48:09 +0530

    changes to projects

--- src/routes/pages/projects.svelte
+++ src/routes/pages/projects.svelte
@@ -1,54 +1,88 @@
 <script lang="ts">
 	// @ts-nocheck
+	import home from '../res/projects/home5559.webp';
+	import assetify from '../res/projects/assettify.webp';
+	import fastfinger from '../res/projects/fastfingers.webp';
+	import weather from '../res/projects/weather.webp';
+	import todo from '../res/projects/todoflutter.webp';
+	import bmicalc from '../res/projects/bmicalculator.webp';
+	import memmorymaster from '../res/projects/memmorymaster.webp';
+	import mytimer from '../res/projects/mytimer.webp';
+	import mybooks from '../res/projects/mybooks.webp';
 	let projectlist = [
 		[
 			'Home',
-			'Fully customisable browser homepage',
-			'https://picsum.photos/200/300',
+			'Homepage is a webApp that is meant to replace your default browser homepage, with a more customizable and user friendly version that syncs across devices.',
+			 home,
+			'https://github.com/SreejithKSGupta/Homepage',
 			'https://home5559.web.app',
-			'https://github.com/sreejithks/home5559',
-			['Sign in with google', 'Fully Customisable', 'Sync across devices', 'No Ads']
+			['HTML,CSS,JS,', 'Sign in with google', 'Fully Customisable', 'Sync across devices', 'No Ads']
 		],
 		[
-			'Home',
-			'Fully customisable browser homepage with 0 Ads 0 restrictions  and 100% Fun, and also, it syncs across all your devices to give you a uniform experience',
-			'https://picsum.photos/200/300',
-			'https://home5559.web.app',
-			'https://github.com/sreejithks/home5559',
-			['Sign in with google', 'Fully Customisable', 'Sync across devices', 'No Ads']
+			'Assettify',
+			'group project with a modern take on helping the young generations build good financial management skills, through gamification',
+			 assetify,
+			'https://github.com/SreejithKSGupta/AssetiFii',
+			'https://assetifii.web.app/',
+			['HTML,CSS,JS,', 'Sign in with google']
 		],
 		[
-			'Home',
-			'Fully customisable browser homepage with 0 Ads 0 restrictions  and 100% Fun, and also, it syncs across all your devices to give you a uniform experience',
-			'https://picsum.photos/200/300',
-			'https://home5559.web.app',
-			'https://github.com/sreejithks/home5559',
-			['Sign in with google', 'Fully Customisable', 'Sync across devices', 'No Ads']
+			'Fast Fingers',
+			'Typing game to improve typing speed in a fun way, that rewards you for your efforts.',
+			 fastfinger,
+			'https://github.com/SreejithKSGupta/FastFingers',
+			'https://sreejithksgupta.github.io/FastFingers/',
+			['HTML,CSS,JS,']
 		],
 		[
-			'Home',
-			'Fully customisable browser homepage with 0 Ads 0 restrictions  and 100% Fun, and also, it syncs across all your devices to give you a uniform experience',
-			'https://picsum.photos/200/300',
-			'https://home5559.web.app',
-			'https://github.com/sreejithks/home5559',
-			['Sign in with google', 'Fully Customisable', 'Sync across devices', 'No Ads']
+			'Weather',
+			'Weather app using openweathermap api, manual/automatic location slection',
+			 weather,
+			'https://github.com/SreejithKSGupta/weather-app',
+            'https://sreejithksgupta.github.io/weather-app/',
+			['HTML,CSS,JS,Open Weather Map API']
 		],
 		[
-			'Home',
-			'Fully customisable browser homepage with 0 Ads 0 restrictions  and 100% Fun, and also, it syncs across all your devices to give you a uniform experience',
-			'https://picsum.photos/200/300',
-			'https://home5559.web.app',
-			'https://github.com/sreejithks/home5559',
-			['Sign in with google', 'Fully Customisable', 'Sync across devices', 'No Ads']
+			'Todo',
+			'Todo app using flutter, with material themes and Fluid animations',
+			 todo,
+			'https://github.com/SreejithKSGupta/mytodoo',
+			'https://github.com/SreejithKSGupta/mytodoo/releases/download/initial/app-release.apk',
+			['Flutter' ,'Material 3','Responsive', 'APK and EXE files available' ]
 		],
 		[
-			'Home',
-			'Fully customisable browser homepage with 0 Ads 0 restrictions  and 100% Fun, and also, it syncs across all your devices to give you a uniform experience',
-			'https://picsum.photos/200/300',
-			'https://home5559.web.app',
-			'https://github.com/sreejithks/home5559',
-			['Sign in with google', 'Fully Customisable', 'Sync across devices', 'No Ads']
-		]
+			'BMI Calculator',
+			'BMI Calculator using flutter, with real time changing slider',
+			 bmicalc,
+			'https://github.com/SreejithKSGupta/mybmi',
+			'https://github.com/SreejithKSGupta/mybmi/releases/download/initial/MyBmi.apk',
+			['Flutter' ,'Material 3','real time slider', 'APK and EXE files available' ]
+		],
+		[
+			'Memmory Master',
+			'Simple card opening Game using vanilla JS, with level adjustments',
+			 memmorymaster,
+			'https://github.com/SreejithKSGupta/memorymaster',
+			'https://sreejithksgupta.github.io/memorymaster/',
+			['HTML, CSS,JS', 'Responsive', 'Level adjustments', ]
+		],
+		[
+			'My Timer',
+			'Timer app built using vanilla JS, with gamified UI that tracks the time spent and awards points',
+			 mytimer,
+			'https://github.com/SreejithKSGupta/Timer',
+			'https://sreejithksgupta.github.io/Timer/',
+			['HTML, CSS,JS' ,'colourfull UI','Gamified' ]
+		],
+		[
+			'My Books',
+			'Book finder app build using JS and google books api, with a responsive UI',
+		 	 mybooks,
+			'https://github.com/SreejithKSGupta/MyBooks',
+			'https://sreejithksgupta.github.io/MyBooks/',
+			['HTML ,CSS ,JS' ,'Material theme','Responsive', 'Google books api' ]
+		],
+		
 	];
 </script>
 
@@ -57,7 +91,7 @@
 		<div class="projectitem">
 			<img class="projectimg" src={project[2]} alt={project[0]} />
 			<div class="projectdetails">
-				<div class='h4'>{project[0]}</div>
+				<div class="h4">{project[0]}</div>
 				<p>{project[1]}</p>
 				<ul>
 					{#each project[5] as features}
@@ -66,11 +100,11 @@
 				</ul>
 				<div class="btnrow">
 					{#if project[4] != ''}
-						<a href={project[4]}
+						<a href={project[3]}
 							><div class="projectbtn" style="background-color: green;">Github</div></a
 						>{/if}
 					{#if project[3] != ''}
-						<a href={project[3]}
+						<a href={project[4]}
 							><div class="projectbtn" style="background-color: blue;">Live</div></a
 						>{/if}
 				</div>
